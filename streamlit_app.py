b8b966f49959c7b945bfc79dcd4d02e610387f7d
Nithin Renjan <123060859+nithinrenjan@users.noreply.github.com>
2023-04-20 17:22:12 +0530

    Update streamlit_app.py
    changed according Snowflake tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title("Hi, I'm Nithin Renjan.!!")
-streamlit.header("I'm a Software Engineer")
-streamlit.text("Skills:")
-streamlit.text("Full Stack Java\tSnowflake")
+streamlit.title("My Parents New Healthy Diner!")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
