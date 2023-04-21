d4281c3562a2e9c94d4fe35b23f28fadef031d8d
Nithin Renjan <123060859+nithinrenjan@users.noreply.github.com>
2023-04-21 18:07:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,21 +22,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
+#create the repeatable code block/function
+def get_fruityvice_data(the_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Takes the json version of data and normalizes it.
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 # New section to diplay fruitvice api response
 streamlit.header("Fruitvice Fruit Advice!")
 
 #just displays the json data to screen
 #streamlit.text(fruityvice_response.json()) 
 
-#added a user text input
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
     else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      # Takes the json version of data and normalizes it.
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
       # Outputs into the screen as table
       streamlit.dataframe(fruityvice_normalized)
       streamlit.write('The user entered ', fruit_choice)
