298d6cf802cce08269345a22d950c4f3b433bf26
Nithin Renjan <123060859+nithinrenjan@users.noreply.github.com>
2023-04-21 17:50:46 +0530

    Update streamlit_app.py
    testing an update of the input text responsiveness with table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,15 @@ streamlit.dataframe(my_fruit_list)
 # New section to diplay fruitvice api response
 streamlit.header("Fruitvice Fruit Advice!")
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
-
 #just displays the json data to screen
 #streamlit.text(fruityvice_response.json()) 
 
 #added a user text input
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+abc="https://fruityvice.com/api/fruit/"+fruit_choice
+fruityvice_response = requests.get(abc)
 
 # Takes the json version of data and normalizes it.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
