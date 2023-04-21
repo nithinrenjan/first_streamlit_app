25de8f6eb3f68392011cdee412be6377f1c61d7a
Nithin Renjan <123060859+nithinrenjan@users.noreply.github.com>
2023-04-21 15:51:37 +0530

    Update streamlit_app.py
    hided the json data display using #

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,9 @@ streamlit.header("Fruitvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just displays the json data to screen
+
+#just displays the json data to screen
+#streamlit.text(fruityvice_response.json()) 
 
 # Takes the json version of data and normalizes it.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
